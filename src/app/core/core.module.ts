1355646c71a25b03632e9049496b87f461ccd84a
Weverton Souza <wevertonad@gmail.com>
2020-01-05 14:39:42 -0300

    first commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EntityDataModule } from '@ngrx/data';
+import { entityConfig } from '../entity-metadata';
+import { environment } from '@environments/environment';
+import { MAT_DATE_LOCALE } from '@angular/material';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from "@shared/service/in-memory-data.service";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    EntityDataModule.forRoot(entityConfig),
+    StoreModule.forRoot([]),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
+    StoreDevtoolsModule.instrument(),
+    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 50 }) : [],
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
+  ],
+  exports: [
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule,
+    EffectsModule,
+    StoreDevtoolsModule,
+    StoreRouterConnectingModule,
+    EntityDataModule
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+  ],
+})
+export class CoreModule { }
