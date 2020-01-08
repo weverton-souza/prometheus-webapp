e01b4b7321e9ff2a1c20080e8c4c8522a1dff78e
Weverton Souza <wevertonad@gmail.com>
2020-01-08 09:21:17 -0300

    created notification service and iniated user state.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UpdateProfile, CreateProfile, DeleteProfile } from '@shared/app-state/user/user.actions';
+import { Profile } from '@shared/model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'prometheus-webapp';
+export class AppComponent implements OnInit{
+  title = 'Prometheus';
+  
+  ngOnInit() {
+    console.log(new UpdateProfile(new Profile()));
+    console.log(new DeleteProfile(new Profile()));
+  }
 }
